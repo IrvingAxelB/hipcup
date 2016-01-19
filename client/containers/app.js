6c6dc619298b1d98527e87b8effa8498459bc2e4
msarnicki <melindasarnicki@gmail.com>
2016-01-18 20:13:15 -0800

    [Progress] Adds router

--- client/containers/app.js
+++ client/containers/app.js
@@ -1,9 +1,30 @@
-import React from 'react'
-import {render} from 'react-dom'
-import Hipcup from '../components/hipcup.js'
-import * as keys from '../../config.js'
 
-render(
-  <Hipcup googleKey={ keys.google_api_key } />,
-  document.getElementById('app')
-);
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, IndexRoute,IndexLink } from 'react-router'
+import { createHistory, useBasename } from 'history'
+import Header from '../components/header'
+import Footer from '../components/footer'
+
+class Main extends React.Component {
+
+  render() {
+    return <div>
+      <Header />
+      {this.props.children}
+      <Footer />
+    </div>
+  }
+}
+
+const history = useBasename(createHistory)({
+  basename: '/'
+})
+
+render((
+  <Router history={history}>
+    <Route path="/" component={Main}>
+    </Route>
+  </Router>
+), document.getElementById('app'))
+
